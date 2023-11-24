2fe1d58123d868a75fa29fb5e01fc1de686d4891
gdpsbotbymark <122553112+gdpsbotbymark@users.noreply.github.com>
2023-11-24 20:02:46 +0200

    Add files via upload

---
+++ android/games.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+<html>
+
+<head>
+    <title>2B-MarkGD Web-Sate</title>
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body style="background: linear-gradient(#000000, #16008f);">
+    <center>
+        <img src="/logo/logo.png" style="max-width: 40%; height: auto;">
+</h1>
+<div>
+<h1 style="
+color: white;
+"><p>Извените но этой игры нету на PC</p></h1>
+
+<div class="button">                                      
+ <a class='knopka' href="/pc/pc.html">       
+ <img src="back.png">
+
+</a>
+
+</h1>
+<div>
+<h1 style="
+color: white;
+"><p>2B-MarkGD © 2023. Сайт сделал 2B-MarkGD.</p></h1>
+
+        <div class="text">
+        </div>
+    </center>
+</body>
+
+</html>
